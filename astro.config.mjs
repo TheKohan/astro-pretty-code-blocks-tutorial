b7fab5b007cce74fc2fd272007dfe4a368823d48
TheKohan <kohan142011@gmail.com>
2023-04-22 19:02:03 +0200

    feat: rehype-pretty-code setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,36 @@
 import { defineConfig } from 'astro/config';
+import rehypePrettyCode from "rehype-pretty-code";
+import json from "./public/moon_ii.json" assert { type: "json" };
 
+ 
+const options = {
+  // Specify the theme to use or a custom theme json, in our case
+  // it will be a moonlight-II theme from
+  // https://github.com/atomiks/moonlight-vscode-theme/blob/master/src/moonlight-ii.json
+  theme: json,
+  // Callbacks to customize the output of the nodes
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{type: 'text', value: ' '}];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    // Adding a class to the highlighted line
+    node.properties.className.push('highlighted');
+  },
+  onVisitHighlightedWord(node) {
+    // Adding a class to the highlighted word
+    node.properties.className = ['word'];
+  },
+};
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown:{
+        syntaxHighlight: false,
+        rehypePlugins: [
+            [rehypePrettyCode, options],
+        ],      
+    },
+});
